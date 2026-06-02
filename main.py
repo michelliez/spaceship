e03c4ebc11e7744f4de95a68f7cee77b861dd132
michelliez <michellezhou7788@gmail.com>
2026-06-02 17:08:19 -0400

    Dataframe

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 print(os.environ['KAGGLE_USERNAME'])
+print(os.environ['KAGGLE_KEY'])
 
 
 import sys
 import kagglehub
 
-dataset_name = sys.argv[1]
-dataset = kagglehub.competition_download('dataset_name', path='./data', unzip = True) #competitions/spaceship-titanic
+#dataset_name = sys.argv[1]
+#path_name = sys.argv[2]
+
+dataset = kagglehub.competition_download('spaceship-titanic') 
+    #competitions/spaceship-titanic train.csv
+    
+# print (dataset)
+# print(os.listdir(dataset))
 
 
 import pandas as pd
-df = pd.read_csv(dataset)
+train_file = dataset + '/train.csv'
+test_file = dataset + '/test.csv'
+train_df = pd.read_csv(train_file)   
